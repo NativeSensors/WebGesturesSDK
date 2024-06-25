24bcbd0c932e2595b2f02b0c09696a56ce1f6c15
Wallace <piotr.walas.prywatny@gmail.com>
2024-06-25 23:42:09 +0200

    [WiP] this is still not working

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # flask_app.py
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+import gevent
 import time
 
 flask_app = Flask(__name__)
-socketio = SocketIO(flask_app)
+flask_app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(flask_app, cors_allowed_origins="*")
 
 # Serve the HTML page with JavaScript for WebSocket communication
 
@@ -24,8 +26,9 @@ def eyecanvas():
 
 # realtime comms
 @socketio.on('connect')
-def on_connection():
-    socketio.emit('rsp', {"DATA": "echo"})
+def on_connect():
+    for n in range(10):
+        emit('rsp', {"DATA": "echo"})
 
 # Handle WebSocket connections
 count_received = 0
@@ -33,7 +36,7 @@ count_received = 0
 def on_stream(clientData):
     global count_received
     print(f"on stream {count_received}")
-    socketio.emit('rsp', {"DATA": "echo"})
+    emit('rsp', {"DATA": "echo"})
     count_received += 1
 
 if __name__ == '__main__':
--- templates/sdk/eyeCanvas.js
+++ templates/sdk/eyeCanvas.js
@@ -1,24 +1,19 @@
 
 // Create socket for cooms
 var eyeGesturesDomain = "{{domain}}";
-var addr = 'http://' + eyeGesturesDomain;
-const socket =  io.connect(addr);
-
-/* ============================================= API INTERNALS ======================================================= */
+var addr = 'ws://' + eyeGesturesDomain;
+var socket =  io.connect(addr);
 
 socket.on('connect', function()
 {
-    console.log(`Connection established. socket.id: ${socket.id}`);   
+    console.log(`Connection established. socket.id: ${socket.id}`);
 });
 
-socket.on('rsp', (message) => {
-    console.log(`Message from server. socket.id: ${socket.id}`);
-});
 
 var count_sent = 0;
 function sendFrame()
 {
-    console.log("sending message: "+count_sent);
+    console.log("sending message: " + count_sent);
     socket.emit('msg', {
         "DUMMY" : "DATA"
     });
@@ -30,6 +25,9 @@ function loop()
     sendFrame();
 }
 
-frameInterval = 1000;
+var frameInterval = 1000;
 setInterval(loop,frameInterval);
 
+socket.on('rsp', (message) => {
+    console.log(`Message from server. socket.id: ${socket.id}`);
+});
