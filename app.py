c7c6201bcbb480b9c2bc2a1c2d4b0777cf95d666
Wallace <piotr.walas.prywatny@gmail.com>
2024-06-25 22:43:19 +0200

    cleaning a lot of stuff

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 # flask_app.py
-from flask import Flask, redirect, render_template, request, send_from_directory, send_file
-from flask_cors import CORS
-# from celery import Celery
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-from logging.config import dictConfig
-from datetime import datetime
-import threading  
-import logging
-# import tasks
-import time
-import os
-
-# CLIENTS_LIMIT = 49
-# SILENT_KEY = "v49e$8gGeNV3ve3xfwuPStt82bM@nSMVUeJF36LM4aTZ!^q87@G^6xtxJUqM"
 
 flask_app = Flask(__name__)
 socketio = SocketIO(flask_app)
@@ -20,208 +8,22 @@ socketio = SocketIO(flask_app)
 # Serve the HTML page with JavaScript for WebSocket communication
 @flask_app.route('/')
 def index():
-
-    # if(len(tasks.get_clients()) > CLIENTS_LIMIT):
-    #     return render_template('we_are_at_full_capacity.html')
-
-    user_agent = request.headers.get('User-Agent')
-    user_agent = user_agent.lower()
-
-    # prepare unique_id and key and set key into dict
-    if "iphone" in user_agent:
-        return render_template('we_are_at_ful_capacity.html')
-    elif "android" in user_agent:
-        return render_template('we_are_at_ful_capacity.html')
-    else:
-        return redirect('/v2_test')
-        # return render_template('main.html')
-
-# Serve the HTML page with JavaScript for WebSocket communication
-@flask_app.route('/debug')
-def debug():
-    # prepare unique_id and key and set key into dict
-    return render_template('main_DEBUG.html')
-
-# Serve the HTML page with JavaScript for WebSocket communication
-@flask_app.route('/game')
-def demo_game():
-    return render_template('demos/game.html')
-
-# Serve the HTML page with JavaScript for WebSocket communication
-@flask_app.route('/cinema')
-def demo_cinema():
-    return render_template('demos/cinema.html')
-
-# Serve the HTML page with JavaScript for WebSocket communication
-@flask_app.route('/v2_test')
-def v2_test():
     return render_template('v2_test.html')
-
-
-@flask_app.route('/restaurant')
-def demo_restaurant():
-    return render_template('demos/restaurant.html')
 
 @flask_app.route('/eyeCanvas.js')
 def eyecanvas():
     # unique_id = tasks.generater_ID()
     return render_template('sdk/eyeCanvas.js', domain = request.host, unique_id = "unique_id")
 
-
-@flask_app.route('/eyegestures.js')
-def webgestures():
-    # unique_id = tasks.generater_ID()
-    return render_template('sdk/eyegestures.min.js', domain = request.host, unique_id = "unique_id")
-
-@flask_app.route('/eyeTiles.js')
-def webTiles():
-    # unique_id = tasks.generater_ID()
-    return render_template('sdk/eyeTiles.min.js', domain = request.host, unique_id = "unique_id") 
-
-@flask_app.route('/eyeMagnet.js')
-def eyeMagnet():
-    # unique_id = tasks.generater_ID()
-    return render_template('sdk/eyeMagnet.min.js', domain = request.host, unique_id = "unique_id")
-
-@flask_app.route('/eyeGest.js')
-def eyeGest():
-    # unique_id = tasks.generater_ID()
-    return render_template('sdk/eyeGest.min.js', domain = request.host, unique_id = "unique_id")
-
-@flask_app.route('/vision/<path:name>')
-def vision(name):
-    if 'css' in name:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'), name)
-    elif name == "eyeGest.js":
-        # unique_id = tasks.generater_ID()
-        return render_template('sdk/eyeGest.min.js', domain = request.host, unique_id = "unique_id")
-    else:
-        return render_template(f"vision/{name}")
-
-
-@flask_app.route('/styles.css')
-def styles():
-
-    user_agent = request.headers.get('User-Agent')
-    user_agent = user_agent.lower()
-
-    logging.info(f'{datetime.now().strftime("%m:%d:%Y:%H:%M:%S")} emiting styles ti ua: {user_agent}')
-    if "iphone" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'mobile.styles.css')
-    elif "android" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'mobile.styles.css')
-    else:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'styles.css')
-
-@flask_app.route('/eyegestures.css')
-def eyegestures_styles():
-
-    user_agent = request.headers.get('User-Agent')
-    user_agent = user_agent.lower()
-
-    logging.info(f'{datetime.now().strftime("%m:%d:%Y:%H:%M:%S")} emiting styles ti ua: {user_agent}')
-    if "iphone" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyegestures.mobile.styles.css')
-    elif "android" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyegestures.mobile.styles.css')
-    else:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyegestures.styles.css')
-
-@flask_app.route('/eyeTiles.css')
-def eyeTiles_styles():
-
-    user_agent = request.headers.get('User-Agent')
-    user_agent = user_agent.lower()
-
-    logging.info(f'{datetime.now().strftime("%m:%d:%Y:%H:%M:%S")} emiting styles ti ua: {user_agent}')
-    if "iphone" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyeTiles.mobile.styles.css')
-    elif "android" in user_agent:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyeTiles.mobile.styles.css')
-    else:
-        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'eyeTiles.styles.css')
-
-
-@flask_app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(flask_app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@flask_app.route('/UpDown.ico')
-def upDown():
-    return send_from_directory(os.path.join(flask_app.root_path, 'static'),
-                               'UpDown.ico', mimetype='image/vnd.microsoft.icon')
-
-@flask_app.route('/LeftRight.ico')
-def leftRight():
-    return send_from_directory(os.path.join(flask_app.root_path, 'static'),
-                               'LeftRight.ico', mimetype='image/vnd.microsoft.icon')
-
-@flask_app.route('/Calib.ico')
-def Calib():
-    return send_from_directory(os.path.join(flask_app.root_path, 'static'),
-                               'Calib.ico', mimetype='image/vnd.microsoft.icon')
-
-@flask_app.route('/license')
-def license():
-    return render_template('license.html')
-
-@flask_app.route('/demo_pic/<path:name>')
-def demo_pics(name):
-    ret = send_from_directory(os.path.join(flask_app.root_path, 'static/demo_pic'),f'{name}')
-    return ret
-
-@flask_app.route('/download', methods=['POST'])
-def download_file():
-    exe_file = request.form.get('executable')
-
-    if exe_file == 'linux':
-        return send_from_directory(os.path.join(flask_app.root_path, 'static/exe'), 'eyegestures_linux')
-    elif exe_file == 'windows':
-        return send_from_directory(os.path.join(flask_app.root_path, 'static/exe'), 'eyegestures_win.exe')
-    else:
-        # Handle the case where the form is submitted without selecting a file
-        return "Invalid request"
-
 # realtime comms
 @socketio.on('connect')
 def on_connection():
-    print("socket id: ",request.sid)
-    # Broadcast the received image data to all connected clients
-    logging.info(f'{datetime.now().strftime("%m:%d:%Y:%H:%M:%S")} new connection')
-
-    socketio.emit('cursor',{})
-
-@socketio.on('disconnect')
-def on_disconnect():
-    print("socket id: ",request.sid)
-    print("disconnected ================== <<< ")
-    emit('cursor', {})
-    
+    emit('cursor', {"DATA":"echo"})
 
 # Handle WebSocket connections
-@socketio.on('/api/clientData')
-def on_clientData(clientData):
-    logging.info(f'{datetime.now().strftime("%m:%d:%Y:%H:%M:%S")} new connection')
-    print("on client data")
-    # tasks.client_create(clientData,request)
-
-# Handle WebSocket connections
-# @socketio.on('/api/stream')
-# def handle_image_v1(clientData):
-    # payload = tasks.client_process_data(clientData)
-    # print(f"sid: {request.sid}")
-    # emit('/api/cursor',payload, room=request.sid)
-
-
-# Handle WebSocket connections
-@socketio.on('/api_v2/stream')
-def handle_image_v2(clientData):
-    print(f"trying to emit. Session id: {request.sid}")
-    emit('cursor', {})
+@socketio.on('stream')
+def on_stream(clientData):
+    emit('cursor', {"DATA":"echo"})
     
 if __name__ == '__main__':
-    logging.basicConfig(filename='error.log',level=logging.DEBUG)
-    flask_app.run(debug=True)
-    # t1.join()
-    # socketio.run(app=flask_app,debug=True)
+    flask_app.run()
--- templates/sdk/eyeCanvas.js
+++ templates/sdk/eyeCanvas.js
@@ -1,159 +1,37 @@
 
-const offset = {
-    x : 0,
-    y : 0
-};
+// Create socket for cooms
+var eyeGesturesDomain = "{{domain}}";
+var addr = 'http://' + eyeGesturesDomain;
+const socket =  io.connect(addr);
 
-function createCalibrator(className, textContent) {
-    const calibrator = document.createElement("div");
-    calibrator.className = className;
-    calibrator.textContent = textContent;
-    return calibrator;
-}
+/* ============================================= API INTERNALS ======================================================= */
 
-function display_calibrator(name){
-    const element = createCalibrator("calibrator calibrator_"+`${name}`+" message-"+`${name}`,"Move cursor here");
-    const background = document.createElement("div");
-    background.className = "calibration_background";
-    document.body.appendChild(element);
-    document.body.appendChild(background);
-}
-
-var calibration_counter = 0;
-var calibration_sequence = [];
-var startTimestamp = Date.now();
-const MIN_TIME_WAIT = 2000;//ms
-
-function createVideoElement(){
-    let video = document.createElement("video");
-    video.id = "localVideo";
-    video.width = 360;
-    video.height = 240;
-    video.autoplay = true;
-    video.muted = true;
-    video.hidden = true;
-    video.style.width = "auto";
-    video.style.height = "auto";
-    return video;
-}
-
-function createTransportCanvas(){
-    // Send each frame to the server
-    trasnportCanvas = document.createElement('canvas');
-    trasnportCanvas.width = 640;
-    trasnportCanvas.height = 360;
-    // return trasnportCanvas.getContext('2d');
-    return trasnportCanvas;
-}
-
-
-
-function EyeCanvasApi(API_KEY, thresh, radius, options = {})
+socket.on('connect', function()
 {
-    const {
-        display_width = document.body.clientWidth - 75,
-        display_height = document.body.clientHeight - 75,
-        onFrame = function(video){},
-        onCursor = function(x,y,fix,blink){},
-        onCalibration = function(){},
-        onWeakConnection = function(){},
-        calibration_layout = true,
-        calibration = true,
-        DEBUG = false
-    } = options;
+    console.log(`Connection established. socket.id: ${socket.id}`);        
+});
 
-    // Create socket for cooms
-    var eyeGesturesDomain = "{{domain}}";
-    var addr = 'http://' + eyeGesturesDomain;
-    const socket =  io.connect(addr);
-    console.log("socket.id: ", socket.id);
+socket.on('cursor', (message) => {
+    console.log(`Message from server. socket.id: ${socket.id}`);
+});
 
-    socket.on("connect_error", (err) => {
-        console.log(`connect_error due to ${err.message}`);
+function sendFrame(){
+
+    console.log("sending message");
+    socket.emit('stream', {
+        "DUMMY" : "DATA"
     });
-
-    var unique_id = "{{unique_id}}";  
-    // Set attributes for the video element
-    const video = createVideoElement();
-    // Append the video element to the body or any other container
-    document.body.appendChild(video);
-    // Create transport view for changing video frame into sendable item
-    const trasnportCanvas = createTransportCanvas();
-
-    /* ============================================= API INTERNALS ======================================================= */
-
-
-    socket.on('connect', function()
-    {
-        console.log(`Connection established. socket.id: ${socket.id}`);        
-    });
-
-    // Emit the '/api/clientData' event with data as soon as the client connects
-    socket.on('/api/onConnect', function () {
-        console.log("/api/onConnect");
-    });
-
-    socket.on("disconnect", () => {
-        console.log(`disconnected: ${socket.id}`); // undefined
-    });
-
+    
     socket.on('cursor', (message) => {
         console.log(`Message from server. socket.id: ${socket.id}`);
     });
+}
 
-    function sendFrame(){
-        console.log(`sending frame. Socket id: ${socket.id}`);
-        // Flip the image horizontally
-        trasnportContext = trasnportCanvas.getContext('2d');
-        if(DEBUG){
-            var image = new Image();
-            image.src = '/demo_pic/debug_face.jpg';
-            trasnportContext.drawImage(image, 0, 0, trasnportCanvas.width, trasnportCanvas.height);
-            onFrame(image); // for user to draw/display/image on the screen - we use hidden display
+function loop()
+{
+    sendFrame();
+}
 
-        }
-        else{
-            trasnportContext.drawImage(video, 0, 0, trasnportCanvas.width, trasnportCanvas.height);
-            onFrame(video); // for user to draw/display/image on the screen - we use hidden display
-        }
+frameInterval = 1000;
+setInterval(loop,frameInterval);
 
-        const imageData = trasnportCanvas.toDataURL('image/jpeg', 0.8); // Convert frame to base64
-
-        socket.emit('/api_v2/stream', {
-            "key" : API_KEY,
-            "unique_id" : unique_id,
-            "thresh"    : thresh,
-            "radius"    : radius,
-            "height"    : display_height,
-            "width"     : display_width,
-            "offset_x"  : offset.x,
-            "offset_y"  : offset.y,
-            "image"     : imageData ,
-            "timestamp" : Date.now()
-        });
-    }
-
-    function loop()
-    {
-        sendFrame();
-    }
-
-    function startVideoPlayer(video,stream)
-    {
-        // Display the local video stream
-        video.srcObject = stream;
-        video.play();
-    }
-
-    // Send the stream to the server (You need to implement the server-side logic separately)
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream){
-        startVideoPlayer(video,stream);
-
-        // start sending loop
-        const frameInterval = 1000; // Adjust the frame rate as needed
-        setInterval(loop,frameInterval);
-    }).catch(function(error){
-        console.error('Error accessing the camera and microphone:', error);
-    });
-};
