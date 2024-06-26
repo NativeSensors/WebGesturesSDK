7c8b0e7f83a886afb454696aaa0be3df617d4f95
Wallace <piotr.walas.prywatny@gmail.com>
2024-06-26 15:13:33 +0200

    minimal working websocket eyegestures client

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # flask_app.py
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-import gevent
-import time
+from eyeGestures.eyegestures import EyeGestures_v2
+import numpy as np
+import base64
+import cv2
 
 flask_app = Flask(__name__)
 socketio = SocketIO(flask_app)
+gestures = EyeGestures_v2()
 
 # Serve the HTML page with JavaScript for WebSocket communication
 
@@ -22,20 +25,27 @@ def eyecanvas():
         domain=request.host,
         unique_id="unique_id")
 
-# realtime comms
-@socketio.on('connect')
-def on_connect():
-    for n in range(10):
-        emit('rsp', {"DATA": "echo"})
-
 # Handle WebSocket connections
-count_received = 0
-@socketio.on('msg')
-def on_stream(clientData):
-    global count_received
-    print(f"on stream {count_received}")
-    emit('rsp', {"DATA": "echo"})
-    count_received += 1
+count = 0
+@socketio.on('msg_data')
+def on_stream(data):
+    global count
+    image_data = data['image'].split(',')[1]  # Remove data URI header
+    image_array = np.frombuffer(base64.b64decode(image_data), dtype=np.uint8)
+    frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    try:
+        event, calibration = gestures.step(frame, count < 300, data['width'], data['height'])
+        emit('rsp', {"x" : event.point[0], 
+                    "y" : event.point[1],
+                    "c_x" : calibration.point[0], 
+                    "c_y" : calibration.point[1]})
+        count+=1
+    except:
+        emit('rsp', {"x" : 10, 
+            "y" : 10,
+            "c_x" : 1000, 
+            "c_y" : 500})
 
 if __name__ == '__main__':
-    socketio.run(app=flask_app)
+    socketio.run(app=flask_app,host="localhost",port=8000)
--- templates/sdk/eyeCanvas.js
+++ templates/sdk/eyeCanvas.js
@@ -1,33 +1,83 @@
 
-// Create socket for cooms
-var eyeGesturesDomain = "{{domain}}";
-var addr = 'ws://' + eyeGesturesDomain;
-var socket =  io.connect(addr);
+const cursor = document.createElement('div');
+cursor.setAttribute('id','cursor')
+document.body.appendChild(cursor);
 
-socket.on('connect', function()
-{
-    console.log(`Connection established. socket.id: ${socket.id}`);
-});
+const calib_cursor = document.createElement('div');
+calib_cursor.setAttribute('id','calib_cursor')
+document.body.appendChild(calib_cursor);
 
 
-var count_sent = 0;
-function sendFrame()
-{
-    console.log("sending message: " + count_sent);
-    socket.emit('msg', {
-        "DUMMY" : "DATA"
-    });
-    count_sent+=1;
+function createVideoElement(){
+    let video = document.createElement("video");
+    video.id = "localVideo";
+    video.width = 360;
+    video.height = 240;
+    video.autoplay = true;
+    video.muted = true;
+    video.hidden = true;
+    video.style.width = "auto";
+    video.style.height = "auto";
+    return video;
 }
 
-function loop()
-{
+function createTransportCanvas(){
+    // Send each frame to the server
+    let trasnportCanvas = document.createElement('canvas');
+    trasnportCanvas.width = 640;
+    trasnportCanvas.height = 360;
+    // return trasnportCanvas.getContext('2d');
+    return trasnportCanvas;
+}
+
+const video = createVideoElement();
+// Append the video element to the body or any other container
+document.body.appendChild(video);
+// Create transport view for changing video frame into sendable item
+const trasnportCanvas = createTransportCanvas();
+
+const socket = io();
+
+navigator.mediaDevices.getUserMedia({ video: true })
+.then(function (stream){    
+    startVideoPlayer(video,stream);
+    // start sending loop
     sendFrame();
+}).catch(function(error){
+    console.error('Error accessing the camera and microphone:', error);
+});
+
+
+function sendFrame(){
+    // Flip the image horizontally
+    var trasnportContext = trasnportCanvas.getContext('2d');
+    trasnportContext.drawImage(video, 0, 0, trasnportCanvas.width, trasnportCanvas.height);
+    const imageData = trasnportCanvas.toDataURL('image/jpeg', 0.8); // Convert frame to base64
+    display_width = document.body.clientWidth - 75,
+    display_height = document.body.clientHeight - 75,
+
+
+    socket.emit('msg_data', {
+        "height"    : display_height, 
+        "width"     : display_width,
+        "image"     : imageData ,
+        "timestamp" : Date.now()
+    });
 }
 
-var frameInterval = 1000;
-setInterval(loop,frameInterval);
 
-socket.on('rsp', function(data){
-    console.log(`Message from server. socket.id: ${socket.id}`);
-});
+function startVideoPlayer(video,stream)
+{
+    // Display the local video stream
+    video.srcObject = stream;
+    video.play();
+}
+
+socket.on('rsp', (data) => {
+    cursor.style.left = `${data["x"]}px`;
+    cursor.style.top = `${data["y"]}px`;
+    calib_cursor.style.left = `${data["c_x"]}px`;
+    calib_cursor.style.top = `${data["c_y"]}px`;
+    sendFrame();
+}); 
+
